8789bd914663ae915a0968d8f0a288a17de716b1
Meriem-ing <etudiantemeriem1@gmail.com>
2025-12-13 21:59:39 +0100

    add CORS support

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
 import hashlib
 import os
 
 app = Flask(__name__)
-
+CORS(app)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn():
